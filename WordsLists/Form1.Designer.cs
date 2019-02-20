0dfa805d5c3e567a874282cb85b3a96f6d136269
dany5639 <dany_woozie@hotmail.com>
2019-02-20 01:03:14 +0100

    Initial commit, program is functional.

---
+++ WordsLists/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WordsLists
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column_EN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_FR = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_NL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Notes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button_load = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button_quickSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column_EN,
+            this.Column_FR,
+            this.Column_NL,
+            this.Column_class,
+            this.Notes});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 397);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // Column_EN
+            // 
+            this.Column_EN.HeaderText = "EN";
+            this.Column_EN.Name = "Column_EN";
+            // 
+            // Column_FR
+            // 
+            this.Column_FR.HeaderText = "FR";
+            this.Column_FR.Name = "Column_FR";
+            // 
+            // Column_NL
+            // 
+            this.Column_NL.HeaderText = "NL";
+            this.Column_NL.Name = "Column_NL";
+            // 
+            // Column_class
+            // 
+            this.Column_class.HeaderText = "Class";
+            this.Column_class.Name = "Column_class";
+            // 
+            // Notes
+            // 
+            this.Notes.HeaderText = "Notes";
+            this.Notes.Name = "Notes";
+            // 
+            // button_load
+            // 
+            this.button_load.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_load.Location = new System.Drawing.Point(12, 415);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(75, 23);
+            this.button_load.TabIndex = 2;
+            this.button_load.Text = "Load list";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // button_save
+            // 
+            this.button_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_save.Location = new System.Drawing.Point(93, 415);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 3;
+            this.button_save.Text = "Save list";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button_quickSave
+            // 
+            this.button_quickSave.Location = new System.Drawing.Point(174, 415);
+            this.button_quickSave.Name = "button_quickSave";
+            this.button_quickSave.Size = new System.Drawing.Size(75, 23);
+            this.button_quickSave.TabIndex = 4;
+            this.button_quickSave.Text = "Quick save";
+            this.button_quickSave.UseVisualStyleBackColor = true;
+            this.button_quickSave.Click += new System.EventHandler(this.button_quickSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_quickSave);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.button_load);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "WordsLists";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button_quickSave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_EN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_FR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_NL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_class;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Notes;
+    }
+}
+
---
+++ WordsLists/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordsLists
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, Item> unsortedFilesList2 = new Dictionary<int, Item>();
+        private List<Item> wordsList = new List<Item>();
+        private bool reverseSorting = false;
+        private int[] previousCell = new int[2];
+        private string lastSaveFile = "";
+
+        class Item
+        {
+            public int ID;
+            public string EN;
+            public string FR;
+            public string NL;
+            public string GR; // group
+            public string CT; // notes
+        }
+
+        public enum Columns : byte
+        {
+            EN,
+            FR,
+            NL,
+            GR,
+            CT
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // wordsList.Add(new Item { ID = 0, EN = "aaa", FR = "mot_0_fr", NL = "word_0_nl", JP = "wado_0_jp", GR = "class0", NT = "" }); 
+
+            listView_refresh(dataGridView1);
+
+            // dataGridView1.Columns[0].HeaderText = "test";
+        }
+
+        #region Utilities
+        private static bool debugConsoleWrite = true;
+        private static List<string> LogQ1 = new List<string>();
+
+        public static void WriteCsv(List<string> in_, string file)
+        {
+            var fileOut = new FileInfo(file);
+            if (File.Exists(file))
+                File.Delete(file);
+
+            int i = -1;
+            try
+            {
+                using (var csvStream = fileOut.OpenWrite())
+                using (var csvWriter = new StreamWriter(csvStream))
+                {
+                    foreach (var a in in_)
+                    {
+                        csvStream.Position = csvStream.Length;
+                        csvWriter.WriteLine(a);
+                        i++;
+                    }
+                }
+            }
+            catch
+            { }
+
+        }
+
+        private static List<string> ReadCsv(string filename)
+        {
+            var output = new List<string>();
+
+            using (var reader = new StreamReader(filename))
+            {
+                var line = "";
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+                    output.Add(line);
+                }
+
+                if (output.Last() == null)
+                    output.RemoveAt(output.Count - 1);
+            }
+
+            return output;
+        }
+
+        public static void Log1(string in_, bool force = false)
+        {
+            LogQ1.Add(in_);
+            if (debugConsoleWrite || force)
+                Console.WriteLine($"{in_}");
+        }
+
+        private void WordsListCleanup()
+        {
+            var a = ReadCsv(@"C:\Users\Vanguard\Documents\2019 words.csv");
+            var b = new Dictionary<string, string>();
+
+            foreach (var c in a)
+            {
+                var d = c.Split(",".ToCharArray());
+
+                if (!b.ContainsKey(d[0]))
+                {
+                    if (d.Length > 1)
+                        b.Add(d[0], d[1]);
+                    else
+                        b.Add(d[0], "");
+                }
+                else
+                    if (b[d[0]] == "")
+                    b[d[0]] = d[1];
+
+            }
+
+            foreach (var c in b)
+            {
+                if (c.Value == "")
+                    Log1($"{c.Key}");
+                else
+                    Log1($"{c.Key},{c.Value}");
+
+            }
+
+            WriteCsv(LogQ1, @"C:\Users\Vanguard\Documents\2019words_new.csv");
+            ;
+        }
+
+        #endregion
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            var a = e.Column;
+
+            var b = new List<Item>();
+
+            // There's probably a much shorter way to do this
+            if (a == 0) // column
+            {
+                b = wordsList.OrderBy(x => x.EN).ToList();
+
+                if (reverseSorting)
+                    b = wordsList.OrderBy(x => x.EN).Reverse().ToList();
+            }
+            else if (a == 1)
+            {
+                b = wordsList.OrderBy(x => x.FR).ToList();
+
+                if (reverseSorting)
+                    b = wordsList.OrderBy(x => x.FR).Reverse().ToList();
+            }
+            else if (a == 2)
+            {
+                b = wordsList.OrderBy(x => x.NL).ToList();
+
+                if (reverseSorting)
+                    b = wordsList.OrderBy(x => x.NL).Reverse().ToList();
+            }
+            else if (a == 3)
+            {
+                b = wordsList.OrderBy(x => x.GR).ToList();
+
+                if (reverseSorting)
+                    b = wordsList.OrderBy(x => x.GR).Reverse().ToList();
+            }
+
+            reverseSorting = !reverseSorting;
+
+            wordsList = new List<Item>();
+
+            foreach (var c in b)
+                wordsList.Add(c);
+
+            listView_refresh(dataGridView1);
+        }
+
+        private void listView_refresh(DataGridView listView)
+        {
+            listView.DataSource = null;
+            listView.Rows.Clear();
+
+            var i = -1;
+            foreach (var item in wordsList)
+            {
+                i++;
+                listView.Rows.Add();
+                listView.Rows[i].Cells[0].Value = $"{item.EN}";
+                listView.Rows[i].Cells[1].Value = $"{item.FR}";
+                listView.Rows[i].Cells[2].Value = $"{item.NL}";
+                listView.Rows[i].Cells[3].Value = $"{item.GR}";
+                listView.Rows[i].Cells[4].Value = $"{item.CT}";
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            previousCell = new int[] { dataGridView1.CurrentCell.RowIndex, dataGridView1.CurrentCell.ColumnIndex };
+
+            VerifyDuplicates();
+        }
+
+        private void VerifyDuplicates()
+        {
+            var found = false;
+
+            var prevCellRow = previousCell[0];
+            var prevCellCol = previousCell[1];
+
+            if (prevCellCol > 2)
+                return;
+
+            for (int rowIndex = 0; rowIndex < dataGridView1.Rows.Count - 2; rowIndex++)
+            {
+                if (dataGridView1.Rows[rowIndex].Cells[prevCellCol].Value == null)
+                    continue;
+
+                if (dataGridView1.Rows[rowIndex].Cells[prevCellCol].FormattedValue.ToString() == "")
+                    continue;
+
+                if (dataGridView1.Rows[prevCellRow].Cells[prevCellCol].FormattedValue.ToString() == "")
+                    continue;
+
+                if (prevCellRow == rowIndex && prevCellCol == previousCell[1])
+                    continue;
+
+                var cell = dataGridView1.Rows[rowIndex].Cells[prevCellCol];
+
+                var c = cell.FormattedValue.ToString();
+
+                var d = dataGridView1.Rows[prevCellRow].Cells[prevCellCol].FormattedValue.ToString();
+
+                if (c == d)
+                {
+                    dataGridView1.Rows[prevCellRow].Cells[prevCellCol].ErrorText = "Duplicate";
+                    found = true;
+                }
+            }
+
+            if (!found)
+                dataGridView1.Rows[prevCellRow].Cells[prevCellCol].ErrorText = "";
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            previousCell = new int[] { dataGridView1.CurrentCell.RowIndex, dataGridView1.CurrentCell.ColumnIndex };
+        }
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            wordsList.Clear();
+
+            openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+            if (openFileDialog1.FileName == null)
+                return;
+
+            openFileDialog1.ShowDialog();
+
+            if (!openFileDialog1.FileName.Contains("\\"))
+                return;
+
+            var lines = ReadCsv(openFileDialog1.FileName);
+
+            foreach (var line in lines)
+            {
+                var split = line.Split(",".ToCharArray());
+                var EN = "";
+                var FR = "";
+                var NL = "";
+                var GR = "";
+                var CT = "";
+
+                // This is so nasty
+                if (split.Length > 0)
+                    EN = split[0];
+                if (split.Length > 1)
+                    FR = split[1];
+                if (split.Length > 2)
+                    NL = split[2];
+                if (split.Length > 3)
+                    GR = split[3];
+                if (split.Length > 4)
+                    CT = split[4];
+
+                wordsList.Add(new Item {
+                    EN = EN,
+                    FR = FR,
+                    NL = NL,
+                    GR = GR,
+                    CT = CT });
+
+            }
+
+            listView_refresh(dataGridView1);
+
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName == null)
+                return;
+
+            lastSaveFile = saveFileDialog1.FileName;
+
+            button_quickSave_Click(null, null);
+
+        }
+
+        private void button_quickSave_Click(object sender, EventArgs e)
+        {
+            if (lastSaveFile == "")
+                lastSaveFile = $"{Directory.GetCurrentDirectory()}\\temp.csv";
+
+            if (File.Exists(lastSaveFile))
+                File.Delete(lastSaveFile);
+
+            LogQ1 = new List<string>();
+
+            Log1("EN,FR,NL,Group,Notes");
+
+            for (int row = 0; row < dataGridView1.Rows.Count - 1; row++)
+            {
+                Log1(
+                    $"{dataGridView1.Rows[row].Cells[(int)Columns.EN].FormattedValue.ToString()}," +
+                    $"{dataGridView1.Rows[row].Cells[(int)Columns.FR].FormattedValue.ToString()}," +
+                    $"{dataGridView1.Rows[row].Cells[(int)Columns.NL].FormattedValue.ToString()}," +
+                    $"{dataGridView1.Rows[row].Cells[(int)Columns.GR].FormattedValue.ToString()}," +
+                    $"{dataGridView1.Rows[row].Cells[(int)Columns.CT].FormattedValue.ToString()}" +
+                    "");
+            }
+
+            WriteCsv(LogQ1, lastSaveFile);
+        }
+
+    }
+}
